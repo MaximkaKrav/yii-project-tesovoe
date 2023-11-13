1ea0f83e1206f4098074bee8790122b8f0dd715e
maksimkravcov <isyamises@gmail.com>
2023-11-13 13:52:43 +0300

    +Модальное окно

---
+++ m231110_112138_create_devices_tableTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace console\migrations;
+
+use m231110_112138_create_devices_table;
+use PHPUnit\Framework\TestCase;
+
+class m231110_112138_create_devices_tableTest extends TestCase
+{
+
+}
--- www/yii-project/frontend/views/site/tables.php
+++ www/yii-project/frontend/views/site/tables.php
@@ -10,9 +10,12 @@
 
 
 use frontend\models\TablesDeviceAndStoreModel;
+use yii\bootstrap5\Modal;
 use yii\data\ActiveDataProvider;
 use yii\grid\GridView;
 use yii\helpers\Html;
+
+
 echo Html::a('Добавить', ['create'], ['class' => 'btn btn-success']);
 
 
@@ -28,16 +31,43 @@ echo GridView::widget([
             'attribute' => 'store_id',
             'format' => 'raw',
             'value' => function ($model) {
-                return Html::a($model->store_id, ['store', 'store_id' => $model->store_id]);
+                return Html::a(
+                    $model->store_id,
+                    ['store', 'store_id' => $model->store_id],
+                    [
+                        'data-toggle' => 'modal',
+                        'data-target' => '#store-modal',
+                        'class' => 'modal-link',
+                    ]
+                );
             },
         ],
         'about',
         'created_at',
         ['class' => 'yii\grid\ActionColumn'],
     ],
-
-
 ]);
+
+// Create a Bootstrap Modal for store_id
+Modal::begin([
+    'id' => 'store-modal',
+    'header' => '<h4 class="modal-title">Store Details</h4>',
+]);
+
+echo '<div id="store-modal-content"></div>';
+
+Modal::end();
+
+$js = <<<JS
+    // Attach click event to modal links
+    $('.modal-link').click(function() {
+        var modal = $('#store-modal');
+        modal.find('.modal-body').load($(this).attr('href'));
+    });
+JS;
+
+$this->registerJs($js);
+
 
 
 
